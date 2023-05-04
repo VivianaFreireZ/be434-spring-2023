4b85058792b41a643d086e2d5700b8a7427592cc
VivianaFreireZ <viviana.freire91@outlook.com>
2023-05-03 23:55:14 -0700

    project 1 and 2

---
+++ project/02_substitution/substitution.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+"""
+Author : vfreirezapata <vfreirezapata@localhost>
+Date   : 2023-05-03
+Purpose: Substitution
+"""
+
+import argparse
+import sys
+import random
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='substitution cipher',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('file',
+                        metavar='FILE',
+                        type=argparse.FileType('rt'),
+                        help='Input file')
+
+    parser.add_argument('-s',
+                        '--seed',
+                        help='A random seed',
+                        metavar='SEED',
+                        type=int,
+                        default=3)
+
+    parser.add_argument('-d',
+                        '--decode',
+                        help='A boolean flag',
+                        action='store_true',
+                        default=False)
+
+    parser.add_argument('-o',
+                        '--outfile',
+                        help='Output file',
+                        metavar='FILE',
+                        type=argparse.FileType('wt'),
+                        default=sys.stdout)
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    random.seed(args.seed)
+    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    alpha_random = random.sample(alpha, len(alpha))
+
+    # create dictionary
+    text_dict = {}
+    for i, _ in enumerate(alpha):
+        text_dict[alpha[i]] = alpha_random[i]
+
+    if args.decode:
+        for line in args.file:
+            key_list = list(text_dict.keys())
+            val_list = list(text_dict.values())
+            decoded = []
+            for char in line:
+                if char in alpha:
+                    position = val_list.index(char)
+                    new_letter = key_list[position]
+                else:
+                    new_letter = char
+                decoded.append(new_letter)
+
+            trans = ''.join(decoded)
+            args.outfile.write(trans)
+    else:
+        for line in args.file:
+            line = line.upper()
+            trans = ''.join([text_dict.get(char, char) for char in line])
+            args.outfile.write(trans)
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
